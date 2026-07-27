db60ff50364d014431665e1202e05e48d0bb6794
HuyNgo <huynlhe172025@fpt.edu.vn>
2026-07-27 17:12:01 +0700

    Create service and logic for warehourse

---
+++ BMWMS.Business/Interfaces/IWarehouseService.cs
@@ -0,0 +1,19 @@
+﻿using BMWMS.Business.Common;
+using BMWMS.Business.DTOs.Warehouse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMWMS.Business.Interfaces
+{
+    public interface IWarehouseService
+    {
+        Task<WarehouseResponseDto?> GetByIdAsync(long id);
+        Task<PagedResultDto<WarehouseResponseDto>> GetPagedListAsync(WarehouseFilterDto filter);
+        Task<long> CreateAsync(CreateWarehouseDto dto);
+        Task UpdateAsync(long id, UpdateWarehouseDto dto);
+        Task DeleteAsync(long id);
+    }
+}
