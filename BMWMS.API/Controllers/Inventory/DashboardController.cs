b25340577c718d761885a9d4edd32404b0aed820
HuyNgo <huynlhe172025@fpt.edu.vn>
2026-08-01 15:46:15 +0700

    Create DashboardController.cs

---
+++ BMWMS.API/Controllers/Inventory/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using BMWMS.Business.Interfaces.Inventory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMWMS.API.Controllers.Inventory
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet("summary")]
+        public async Task<IActionResult> GetSummary()
+        {
+            var result = await _dashboardService.GetDashboardSummaryAsync();
+            return Ok(result);
+        }
+    }
+}
