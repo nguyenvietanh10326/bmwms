bcbc41dfbe707e0280b6518f8a552de76031e628
Nguyen Viet Anh <AnhNVHE187352@fpt.edu.vn>
2026-09-19 09:19:53 +0700

    Update transfer form and stock scripts

---
+++ tmp/debug_lowstock.sql
@@ -0,0 +1,22 @@
+﻿SELECT TOP 20
+    p.ProductID,
+    p.ProductCode,
+    p.ProductName,
+    policy.WarehouseID,
+    w.WarehouseCode,
+    policy.MinimumStockQuantity,
+    SUM(i.OnHandQuantity) AS TotalOnHand,
+    SUM(i.ReservedQuantity) AS TotalReserved,
+    SUM(i.AvailableQuantity) AS TotalAvailable,
+    v.AvailableQuantity AS ViewAvailable,
+    v.ShortageQuantity,
+    v.WarehouseCode AS ViewWarehouseCode
+FROM dbo.Products p
+LEFT JOIN dbo.ProductWarehousePolicies policy ON policy.ProductID = p.ProductID
+LEFT JOIN dbo.Warehouses w ON w.WarehouseID = policy.WarehouseID
+LEFT JOIN dbo.Inventory i ON i.ProductID = p.ProductID
+LEFT JOIN dbo.StorageLocations sl ON sl.StorageLocationID = i.StorageLocationID
+LEFT JOIN dbo.vw_LowStockAlerts v ON v.ProductID = p.ProductID AND v.WarehouseID = policy.WarehouseID
+GROUP BY p.ProductID, p.ProductCode, p.ProductName, policy.WarehouseID, w.WarehouseCode,
+         policy.MinimumStockQuantity, v.AvailableQuantity, v.ShortageQuantity, v.WarehouseCode
+ORDER BY p.ProductID;
